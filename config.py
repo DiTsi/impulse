3e9d392f215f1c759cd4539e2dc6aa1102add663
Dmitry Tsybus <tsibous14@gmail.com>
2024-03-06 02:30:43 +0500

    create settings from config.yml

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 import os
+import yaml
+
 
 slack_token = os.environ.get('SLACK_BOT_USER_OAUTH_TOKEN')
+
+
+with open('config.yml', 'r') as file:
+    try:
+        settings = yaml.safe_load(file)
+    except yaml.YAMLError as e:
+        print("Error reading YAML file:", e) #!

cefd6b646169170bf576baaa06dbe06c2e5313ec
Dmitry Tsybus <tsibous14@gmail.com>
2024-07-06 02:07:14 +0500

    Many changes: - merge Slack and Mattermost common functions - buttons in Mattermost works!

--- app/application/mattermost/threads.py
+++ app/application/mattermost/threads.py
@@ -1,97 +1,142 @@
-import json
-from time import sleep
-
-import requests
-
-from .buttons import buttons
-from .config import headers, status_colors
+from config import impulse_url
+from ..colors import status_colors
+from .config import buttons
 
 
-# def create_blocked_thread(channel_id, message, status): #! didn't work with "return modified_message, 200"
-#     payload = {
-#         'channel': channel_id,
-#         'text': '',
-#         "attachments": [{
-#             "color": status_colors.get(status),
-#             "blocks": [
-#                 {
-#                     "type": "section",
-#                     "text": {
-#                         "type": "mrkdwn",
-#                         "text": message
-#                     }
-#                 },
-#                 {
-#                     "type": "divider"
-#                 },
-#                 {
-#                     "type": "actions",
-#                     "elements": [
-#                         {
-#                             "type": "button",
-#                             "text": {
-#                                 "type": "plain_text",
-#                                 "text": "Acknowledge",
-#                                 "emoji": True
-#                             }
-#                         }
-#                     ]
-#                 },
-#                 {
-#                     "type": "context",
-#                     "elements": [
-#                         {
-#                             "type": "mrkdwn",
-#                             "text": "test context"
-#                         }
-#                     ]
-#                 }
-#             ]
-#         }]
-#     }
-#     response = requests.post(
-#         f'{url}/api/chat.postMessage',
-#         headers=headers,
-#         data=json.dumps(payload)
-#     )
-#     return response.json().get('ts')
-def update_thread(url, channel_id, ts, status, message, chain_enabled=True, status_enabled=True):
+def mattermost_get_button_update_payload(message, status, chain_enabled, status_enabled):
+    payload = {
+        'update': {
+            'message': '',
+            'props': {
+                'attachments': [
+                    {
+                        'fallback': 'test',
+                        'text': message,
+                        'color': status_colors.get(status),
+                        'actions': [
+                            {
+                                "id": "chain",
+                                "type": "button",
+                                "name": buttons['chain']['enabled']['text'] if chain_enabled else
+                                buttons['chain']['disabled']['text'],
+                                "style": buttons['chain']['enabled']['style'] if chain_enabled else
+                                buttons['chain']['disabled']['style'],
+                                "integration": {
+                                    "url": f"{impulse_url}/app",
+                                    "context": {
+                                        "action": "chain"
+                                    }
+                                }
+                            },
+                            {
+                                "id": "status",
+                                "type": "button",
+                                "name": buttons['status']['enabled']['text'] if status_enabled else
+                                buttons['status']['disabled']['text'],
+                                "style": buttons['status']['enabled']['style'] if status_enabled else
+                                buttons['status']['disabled']['style'],
+                                "integration": {
+                                    "url": f"{impulse_url}/app",
+                                    "context": {
+                                        "action": "status"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    return payload
+
+
+def mattermost_get_update_payload(channel_id, id, message, status, chain_enabled, status_enabled):
     payload = {
         'channel': channel_id,
-        'text': '',
-        'attachments': [
-            {
-                'color': status_colors.get(status),
-                'text': message,
-                'mrkdwn_in': ['text'],
-            },
-            {
-                'color': status_colors.get(status),
-                'text': f'',
-                "callback_id": "buttons",
-                "actions": [
-                    {
-                        "name": 'chain',
-                        "text": buttons['chain']['enabled']['text'] if chain_enabled else buttons['chain']['disabled']['text'],
-                        "type": 'button',
-                        "style": buttons['chain']['enabled']['style'] if chain_enabled else buttons['chain']['disabled']['style']
-                    },
-                    {
-                        "name": 'status',
-                        "text": buttons['status']['enabled']['text'] if status_enabled else buttons['status']['disabled']['text'],
-                        "type": 'button',
-                        "style": buttons['status']['enabled']['style'] if status_enabled else buttons['status']['disabled']['style'],
-                    }
-                ],
-            },
-        ],
-        'ts': ts,
+        'id': id,
+        'message': '',
+        'props': {
+            'attachments': [
+                {
+                    'fallback': 'test',
+                    'text': message,
+                    'color': status_colors.get(status),
+                    'actions': [
+                        {
+                            "id": "chain",
+                            "type": "button",
+                            "name": buttons['chain']['enabled']['text'] if chain_enabled else
+                            buttons['chain']['disabled']['text'],
+                            "style": buttons['chain']['enabled']['style'] if chain_enabled else
+                            buttons['chain']['disabled']['style'],
+                            "integration": {
+                                "url": f"{impulse_url}/app",
+                                "context": {
+                                    "action": "chain"
+                                }
+                            }
+                        },
+                        {
+                            "id": "status",
+                            "type": "button",
+                            "name": buttons['status']['enabled']['text'] if status_enabled else
+                            buttons['status']['disabled']['text'],
+                            "style": buttons['status']['enabled']['style'] if status_enabled else
+                            buttons['status']['disabled']['style'],
+                            "integration": {
+                                "url": f"{impulse_url}/app",
+                                "context": {
+                                    "action": "status"
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
     }
-    requests.post(
-        f'{url}/api/chat.update',
-        headers=headers,
-        data=json.dumps(payload)
-    )
-    sleep(0.1)
+    return payload
 
 
+def mattermost_get_create_thread_payload(channel_id, message, status):
+    payload = {
+        'channel_id': channel_id,
+        'message': '',
+        'props': {
+            'attachments': [
+                {
+                    'fallback': 'test',
+                    'text': message,
+                    'color': status_colors.get(status),
+                    'actions': [
+                        {
+                            "id": "chain",
+                            "type": "button",
+                            "name": buttons['chain']['enabled']['text'],
+                            "style": "good",  # good, warning, danger, default, primary, and success
+                            "integration": {
+                                "url": f"{impulse_url}/app",
+                                "context": {
+                                    "action": "chain"
+                                }
+                            }
+                        },
+                        {
+                            "id": "status",
+                            "type": "button",
+                            "name": buttons['status']['enabled']['text'],
+                            "style": "good",  # good, warning, danger, default, primary, and success
+                            "integration": {
+                                "url": f"{impulse_url}/app",
+                                "context": {
+                                    "action": "status"
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    return payload

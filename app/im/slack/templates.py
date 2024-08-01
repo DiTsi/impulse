91ec852ed8902bbb95c1c6f6870588e23bc8741f
Alexander <alexander.zhukov0709@gmail.com>
2024-08-01 15:07:04 +0500

    Create templates.py (mattermost needs to be done, slack to be reviewed)

---
+++ app/im/slack/templates.py
@@ -0,0 +1,36 @@
+slack_incident_header_template = """
+{% set status = payload.get("status", "Unknown") -%}
+{% set commonLabels = payload.get("commonLabels", {}) -%}
+{% set status_emoji = {
+   "firing": ":fire:",
+   "resolved": ":white_check_mark:",
+   "Unknown": ":warning:"
+} -%}
+{{ status_emoji.get(status, ":warning:") }} {{ commonLabels.alertname }}
+"""
+
+slack_incident_body_template = """
+{% set status = payload.get("status", "Unknown") -%}
+{% set annotations = payload.get("commonAnnotations", {}).copy() -%}
+{% set commonLabels = payload.get("commonLabels", {}) -%}
+{% set annotations = payload.get("commonAnnotations", {}) -%}
+{% set groupLabels = payload.get("groupLabels", {}) -%}
+{% set commonLabels = payload.get("commonLabels", {}) -%}
+{% set severity = groupLabels.severity -%}
+{% set alerts = payload.get("alerts", {}) -%}
+{% set severity_emoji = {"critical": ":rotating_light:", "warning": ":warning:" }[severity] | default(":question:") -%}
+*{{ annotations.summary }}*
+{% if annotations.description %}_{{ annotations.description }}_{% endif -%}
+{%- if alerts[0].labels.instance %}
+*Instance:* {{ alerts[0].labels.instance }}  {%- for l in alerts[0].labels.keys() if l != 'alertname' and l != 'instance' and l not in commonLabels.keys() -%}{{l}}=`{{ alerts[0].labels[l] }}`{% if not loop.last %},  {% endif %}{% endfor %}
+{%- endif %}
+{%- if annotations.value %}
+*Value:* {{ annotations.value }}
+{%- endif %}
+{%- if commonLabels | length > 0 %}
+*Labels:*
+{%- for k, v in commonLabels.items() if k != 'alertname' and k != 'instance' %}
+   {{ k }}=`{{ v }}`
+{%- endfor %}
+{%- endif %}
+"""

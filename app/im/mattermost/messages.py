9492c0235e4089f1107f473625c27addfaa47ab0
Dmitry Tsybus <tsibous14@gmail.com>
2024-08-07 02:00:38 +0500

    - Fix Mattermost update message API url - Update message looks much better

--- app/im/mattermost/messages.py
+++ app/im/mattermost/messages.py
@@ -4,16 +4,23 @@ from time import sleep
 import requests
 
 from .config import mattermost_headers, mattermost_request_delay
+from ..colors import status_colors
 
 
-def mattermost_send_message(url, channel_id, message):
+def mattermost_send_message(url, channel_id, message, attachment):
     payload = {
-        'channel': channel_id,
-        'text': message,
-        'mrkdwn_in': ['text'],
-        'unfurl_links': False,
-        'unfurl_media': False
+        'channel_id': channel_id,
+        'message': message,
+        'props': {
+            'attachments': [
+                {
+                    'fallback': 'test',
+                    'text': attachment,
+                    'color': status_colors['closed']
+                }
+            ]
+        }
     }
-    response = requests.post(f'{url}/api/chat.postMessage', headers=mattermost_headers, data=json.dumps(payload))
+    response = requests.post(f'{url}/api/v4/posts', headers=mattermost_headers, data=json.dumps(payload))
     sleep(mattermost_request_delay)
     return response.json().get('ts')

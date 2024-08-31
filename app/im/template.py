1426996253bffea72f9c9650a445ed906d521ba9
tansdf <tansdf@mail.ru>
2024-08-31 20:00:10 +0300

    Add ability to use all `incident` attributes in message templates if `incident` is passed to a `form_message`

--- app/im/template.py
+++ app/im/template.py
@@ -1,22 +1,20 @@
 from jinja2 import Template
 
+from app.incident.incident import Incident
+
 
 class JinjaTemplate:
     def __init__(self, template):
         self.template = template
 
-    def form_message(self, alert_state):
+    def form_message(self, alert_state, incident: Incident = None):
         template = Template(self.template)
-        return template.render(payload=alert_state)
+        incident_data = incident.serialize() if incident else {}
+        return template.render(payload=alert_state, incident=incident_data)
 
 
-def generate_template(type_, body_dict=None, header_dict=None, status_icons_dict=None):
-    if type_ == 'slack':
-        incident_body = JinjaTemplate(body_dict)
-        incident_header = JinjaTemplate(header_dict)
-        incident_status_icons = JinjaTemplate(status_icons_dict)
-    else:
-        incident_body = JinjaTemplate(body_dict)
-        incident_header = JinjaTemplate(header_dict)
-        incident_status_icons = JinjaTemplate(status_icons_dict)
+def generate_template(body_dict=None, header_dict=None, status_icons_dict=None):
+    incident_body = JinjaTemplate(body_dict)
+    incident_header = JinjaTemplate(header_dict)
+    incident_status_icons = JinjaTemplate(status_icons_dict)
     return incident_body, incident_header, incident_status_icons

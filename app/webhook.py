9882c0a1f1ac7367f162a8f6ad4ad90dc40b2e2f
tansdf <tansdf@mail.ru>
2024-09-08 14:24:55 +0300

    Changed the Webhook logic to pass incident on Push and render the content on push as well Kept the compatibility

--- app/webhook.py
+++ app/webhook.py
@@ -4,34 +4,39 @@ import requests
 from jinja2 import Template
 from requests.auth import HTTPBasicAuth
 
+from app.incident.incident import Incident
+
 
 class Webhook:
     def __init__(self, url, data=None, auth=None):
-        self.url = self.render(url)
-        for k in data.keys():
-            data[k] = self.render(data[k])
-        self.data = data
-        self.auth = auth
+        self._url = self.render(url)
+        self._pre_render_data = data
+        self._auth = auth
 
-    def push(self):
-        if self.auth is not None:
-            u, p = self.auth.split(':')
+    def push(self, incident: Incident = None):
+        rendered_data = dict()
+        if self._pre_render_data:
+            serialized_incident = incident.serialize() if incident else dict()
+            for key, value in self._pre_render_data.items():
+                rendered_data[key] = self.render(value, incident=serialized_incident)
+        if self._auth is not None:
+            u, p = self._auth.split(':')
             auth = HTTPBasicAuth(self.render(u), self.render(p))
             try:
-                response = requests.post(url=self.url, data=self.data, auth=auth, timeout=1.0)
+                response = requests.post(url=self._url, data=rendered_data, auth=auth, timeout=1.0)
             except requests.exceptions.ConnectionError:
                 return f'Connection Error', None
         else:
             try:
-                response = requests.post(url=self.url, data=self.data, timeout=1.0)
+                response = requests.post(url=self._url, data=rendered_data, timeout=1.0)
             except requests.exceptions.ConnectionError:
                 return f'Connection Error', None
         return 'ok', response.status_code
 
     @staticmethod
-    def render(custom_string):
+    def render(custom_string, **kwargs):
         tmplt = Template(custom_string)
-        return tmplt.render(env=os.environ)
+        return tmplt.render(env=os.environ, **kwargs)
 
 
 def generate_webhooks(webhooks_dict=None):

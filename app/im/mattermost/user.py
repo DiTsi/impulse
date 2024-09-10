9011caf09e3dfeb87d0ab57e4cc93e541ae86763
Dmitry Tsybus <tsibous14@gmail.com>
2024-09-11 01:38:07 +0500

    Fix user notifications for users not in Mattermost

--- app/im/mattermost/user.py
+++ app/im/mattermost/user.py
@@ -17,14 +17,15 @@ class User:
         return self.username
 
     def mention_text(self, admins_usernames):
-        if self.first_name == '' and self.last_name == '':
-            fullname = self.username
-        else:
-            fullname = self.first_name + ' ' + self.last_name
-        text = f'➤ user {mattermost_bold_text(fullname)}: '
-        if self.username:
+        if self.first_name is not None:
+            if self.first_name == '' and self.last_name == '':
+                fullname = self.username
+            else:
+                fullname = self.first_name + ' ' + self.last_name
+            text = f'➤ user {mattermost_bold_text(fullname)}: '
             text += f'{mattermost_mention_text(self.username)}'
         else:
+            text = f'➤ user {mattermost_bold_text(self.username)}: '
             admins_text = mattermost_env.from_string(mattermost_admins_template_string).render(users=admins_usernames)
             text += (f'**not found in Mattermost**\n'
                      f'➤ admins: {admins_text}')

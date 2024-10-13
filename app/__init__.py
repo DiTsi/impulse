e0670f10f4e9f3ecbf54b8afce731d13f03ba181
tansdf <tansdf@mail.ru>
2024-10-14 00:18:22 +0300

    TG Integration P3 - Added buttons to a TG integration - Added button press handling - Refactored handlers a bit

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.im.mattermost.buttons import mattermost_buttons_handler
-from app.im.slack.buttons import slack_buttons_handler
-
-
-def buttons_handler(app, payload, incidents, queue):
-    if app.type == 'slack':
-        return slack_buttons_handler(payload, incidents, queue)
-    else:
-        return mattermost_buttons_handler(app, payload, incidents, queue)
--- app/im/telegram/telegram_application.py
+++ app/im/telegram/telegram_application.py
@@ -2,11 +2,14 @@ import json
 from time import sleep
 
 import requests
+from flask import jsonify
 
 from app.im.application import Application
+from app.im.telegram.config import buttons
 from app.im.telegram.user import User
 from app.logging import logger
 from config import tg_bot_token, impulse_url
+
 
 # Temporary Firing: 🔥, Unknown: ❓, Resolved: ✅, Closed: 🆒
 class TelegramApplication(Application):
@@ -83,6 +86,51 @@ class TelegramApplication(Application):
         message_id = self._send_create_thread(payload)
         return f'{topic_id}/{message_id}'
 
+    def buttons_handler(self, payload, incidents, queue_):
+        if 'callback_query' not in payload:
+            return jsonify({}), 200
+        callback = payload['callback_query']
+        message_id = callback['message']['message_id']
+        post_id = callback['message']['message_thread_id']
+        thread_id = f'{post_id}/{message_id}'
+        incident_ = incidents.get_by_ts(ts=thread_id)
+        action = callback['data']
+        if action in ['start_chain', 'stop_chain']:
+            if action == 'stop_chain':
+                incident_.chain_enabled = False
+                queue_.delete_by_id(incident_.uuid, delete_steps=True, delete_status=False)
+            else:
+                incident_.chain_enabled = True
+                queue_.append(incident_.uuid, incident_.chain)
+        elif action in ['start_status', 'stop_status']:
+            if action == 'stop_status':
+                incident_.status_enabled = False
+            else:
+                incident_.status_enabled = True
+        self.http.post(
+            f'{self.url}/editMessageReplyMarkup',
+            data=json.dumps({
+                'chat_id': callback['message']['chat']['id'],
+                'message_id': message_id,
+                'reply_markup': {
+                    'inline_keyboard': [
+                        [
+                            buttons['chain']['enabled'] if incident_.chain_enabled else buttons['chain']['disabled'],
+                            buttons['status']['enabled'] if incident_.status_enabled else buttons['status']['disabled']
+                        ]
+                    ]
+                }
+            }),
+
+        )
+        self.http.post(
+            f'{self.url}/answerCallbackQuery',
+            data=json.dumps({'callback_query_id': callback['id']}),
+            headers=self.headers
+        )
+        incident_.dump()
+        return jsonify({}), 200
+
     def _create_topic(self, channel_id, header, status_icons):
         payload = {
             'chat_id': channel_id,
@@ -100,12 +148,19 @@ class TelegramApplication(Application):
             logger.error(f'Failed to create topic: {e}')
             raise e
 
-
     def _create_thread_payload(self, channel_id, body, header, status_icons, status):
         return {
             'chat_id': channel_id,
             'text': f'{self._format_tg_icon(status_icons)} {status} {header}\n{body}',
-            'parse_mode': 'MarkdownV2'
+            'parse_mode': 'MarkdownV2',
+            'reply_markup': {
+                'inline_keyboard': [
+                    [
+                        buttons['chain']['enabled'],
+                        buttons['status']['enabled']
+                    ]
+                ]
+            }
         }
 
     def _post_thread_payload(self, channel_id, id_, text):
@@ -148,7 +203,15 @@ class TelegramApplication(Application):
             'chat_id': channel_id,
             'message_id': message_id,
             'text': f'{self._format_tg_icon(status_icons)} {status} {header}\n{body}',
-            'parse_mode': 'MarkdownV2'
+            'parse_mode': 'MarkdownV2',
+            'reply_markup': {
+                'inline_keyboard': [
+                    [
+                        buttons['chain']['enabled'] if chain_enabled else buttons['chain']['disabled'],
+                        buttons['status']['enabled'] if status_enabled else buttons['status']['disabled']
+                    ]
+                ]
+            }
         }
 
     def _update_thread(self, id_, payload):
